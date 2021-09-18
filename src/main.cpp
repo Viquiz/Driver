74d16c325777ee5e0bc1dd07b98ec94efc277f36
Phineasla <63248715+Phineasla@users.noreply.github.com>
2021-09-18 16:32:07 +0700

    Drop password support

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,13 @@ BluetoothSerial SerialBT;
 #include "ClientStorage.h"
 #include "Task.h"
 
-#define PASSWORD_LEN 4
 #define BEACON_INTERVAL (1000 * TASK_MILLISECOND)
 
 void onRecvFromClient(const uint8_t *mac_addr, const uint8_t *data, int data_len);
-uint32_t generatePassword(uint8_t len);
 
 esp_now_peer_info_t broadcastPeer{{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}};
 Scheduler scheduler;
-BeaconPacket broadcastData(generatePassword(PASSWORD_LEN));
+BeaconPacket broadcastData;
 
 void setup()
 {
@@ -37,7 +35,6 @@ void setup()
     ESP.restart();
   }
   LOG_VERBOSE("MAC: %s", WiFi.macAddress().c_str());
-  LOG_VERBOSE("Beacon password: %u", broadcastData.password);
   esp_now_register_recv_cb(&onRecvFromClient);
 
   broadcastPeer.channel = CHANNEL;
@@ -66,29 +63,7 @@ void onRecvFromClient(const uint8_t *mac_addr, const uint8_t *data, int data_len
   case packet_t::RECV_ANSW:
     // Receive client's answer to the question
     break;
-  case packet_t::RESPOND_BEACON:
-    // Receive client's respond to register client
-    break;
-  case packet_t::REQ_BEACON:
-    // client request to know the server address
-    // (in case the client can't capture BeaconPacket)
-    // May not need to implement
-    break;
   default:
     break;
   }
-}
-
-// @param len maximum is 10
-uint32_t generatePassword(uint8_t len)
-{
-  if (len > 10)
-    len = 10;
-  randomSeed(micros());
-  uint32_t password = 0;
-  for (int i = 0; i < len; i++)
-  {
-    password += random(1, 5) * pow(10, i);
-  }
-  return password;
 }
